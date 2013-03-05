569cbd0dd3a2951591386e6eef900f6f478917eb
Gazal Garg <gazal24@gmail.com>
2013-03-05 14:55:48 +0530

    QUERY Case added for remaining files also. Also, now handle(return) separate values for SELECT and UPDATE query.

--- jdbcrmiConnection.java
+++ jdbcrmiConnection.java
@@ -13,14 +13,15 @@ public class jdbcrmiConnection
     {
 	appconfig pinfo = new appconfig();	
 	System.out.println("hello");
-	System.out.println(paramList.get(0));
+	// System.out.println(paramList.get(0));
 	Statement stmt;
 	Connection con;
 	ResultSet rs= null;   
 	String uname_db = "aa";
 	CachedRowSetImpl crs;	
 	String query = "";
-
+	int is_update_query = 0;
+	int query_status = 0;
 	try {
 	    Class.forName(pinfo.jdbccriver);
 	    con = DriverManager.getConnection(pinfo.dburl, pinfo.dbusername, pinfo.dbpasswd); 
@@ -89,18 +90,173 @@ public class jdbcrmiConnection
 		rs = stmt.executeQuery(query);
 		break;
 
-	    }
 
-	    crs = new CachedRowSetImpl();
-	    crs.populate(rs);
-	    
+		// QUERIES FROM game1.jsp
+	    case 50:
+		task_id = (String)paramList.get(0);
+		query = "SELECT * from `method` WHERE task_id=" + task_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+	    case 51:
+		task_id = (String)paramList.get(0);
+		query = "SELECT * from `set` WHERE task_id=" + task_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+
+		// QUERIES FROM result.jsp
+	    case 60:
+		task_id = (String)paramList.get(0);
+		query = "SELECT `score` FROM `game`,`play` WHERE `game`.`task_id` = " + task_id + " AND `play`.`game_id` = `game`.`id`";
+		rs = stmt.executeQuery(query);
+		break;
+
+	    case 61:
+		String play_id = (String)paramList.get(0);
+		query = "SELECT `score` FROM `play` WHERE `id`=" + play_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+
+		// QUERIES FROM creategame.jsp
+	    case 70:
+		task_id = (String)paramList.get(0);
+		query = "Select * from task WHERE id=" + task_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+
+
+		// QUERIES FROM savegame.jsp
+	    case 80:
+		is_update_query = 1;
+		task_id = (String)paramList.get(0);
+		code = (String)paramList.get(1);
+		query = "INSERT INTO `game` (`task_id`, `code`) VALUES ('" + task_id + "', '" + code + "')" ;
+		query_status = stmt.executeUpdate(query);
+		break;
+
+	    case 81:
+		code = (String)paramList.get(0);
+		query = "SELECT id as game_id FROM `game` WHERE `code` = '" + code +"'";
+		rs = stmt.executeQuery(query);
+		break;
+
+	    case 82:
+		is_update_query = 1;
+		game_id = (String) paramList.get(0);
+		code = (String) paramList.get(1);
+		String email = (String) paramList.get(2);
+		query =  "INSERT INTO `play` (`game_id`, `code`, `player_email_id`) VALUES ('" + game_id + "', '" + code + "', '" + email + "')" ;
+		query_status = stmt.executeUpdate(query);
+		break;
+
+
+		// QUERIES FROM savetask.jsp
+	    case 90:
+		is_update_query = 1;
+		uname = (String) paramList.get(0);
+		String tname = (String) paramList.get(1);
+		String mcount = (String) paramList.get(2);
+		query = query =  "INSERT INTO `task` (`uname`, `name`, `method_count`) VALUES ('" + uname + "', '" + tname + "', '" + mcount + "')" ;
+		query_status = stmt.executeUpdate(query);
+		break;
+
+	    case 91:
+		tname = (String) paramList.get(0);
+		query = "SELECT max(id) as task_id FROM `task` WHERE `name` = '" + tname + "'";
+		rs = stmt.executeQuery(query);
+		break;
+
+
+		// QUERIES FROM todo.jsp
+	    case 100:
+		query = "SELECT * from todo ORDER BY `id`";
+		rs = stmt.executeQuery(query);
+		break;
+
+
+		// QUERIES FROM savemethod.jsp
+	    case 110:
+		is_update_query = 1;
+		task_id = (String) paramList.get(0);
+		String mname = (String) paramList.get(1);
+		query =  "INSERT INTO `method` (`task_id`, `name`) VALUES ('" + task_id + "', '" + mname + "')" ;
+		query_status = stmt.executeUpdate(query);
+		break;
+
+
+		// QUERIES FROM savemethod.jsp
+	    case 120:
+		task_id = (String) paramList.get(0);
+		query = "SELECT * FROM `method` WHERE `task_id` =" +task_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+
+		// QUERIES FROM saveimage.jsp
+	    case 130:
+		is_update_query = 1;
+		task_id = (String) paramList.get(0);
+		String sname = (String) paramList.get(1);
+		query = "INSERT INTO `set` (`task_id`, `name`) VALUES ('"+ task_id+"','"+ sname+"')";
+		query_status = stmt.executeUpdate(query);
+		break;
+
+	    case 131:
+		task_id = (String) paramList.get(0);
+		query = "SELECT max(`id`) as set_id FROM `set` WHERE task_id='" + task_id + "'";
+		rs = stmt.executeQuery(query);
+		break;
+
+	    case 132:
+		is_update_query = 1;
+		String set_id = (String) paramList.get(0);
+		set_id = (String) paramList.get(1);
+		query = "UPDATE `set` SET `name` = '"+ set_id +"' WHERE  `set`.`id` ="+set_id+"";
+		query_status = stmt.executeUpdate(query);
+		break;
+
+	    case 133:
+		task_id = (String) paramList.get(0);
+		query = "SELECT * from method WHERE task_id=" + task_id;
+		rs = stmt.executeQuery(query);
+		break;
+
+		// QUERIES FROM savename.jsp
+	    case 140:
+		uname = (String) paramList.get(0);
+		query = "SELECT * from `user` WHERE `uname` = '"+ uname +"'";
+		rs = stmt.executeQuery(query);
+		break;
+		
+	    case 141:
+		is_update_query = 1;
+		uname = (String) paramList.get(0);
+		String name =  (String) paramList.get(1);
+		passwd =  (String) paramList.get(2);
+		query = "INSERT INTO `user` (`uname`, `name`, `password`, `age`) VALUES ('" + uname + "', '" + name + "', '" + passwd + "', '22 ')" ;
+		query_status = stmt.executeUpdate(query);
+
+	    } // END OF CASES
+
 	    XStream xstream = new XStream();
-	    String xml = xstream.toXML(crs); // serialize to XML
+	    String xml="";
+	    if(is_update_query == 1) {
+		xml = xstream.toXML(query_status); // serialize to XML
+	    }
+	    else { //SELECT Query
+		crs = new CachedRowSetImpl();
+		crs.populate(rs);
+		xml = xstream.toXML(crs); // serialize to XML
+	    }
 	    return xml;
 	}
+
 	
 	catch(Exception e){
-	    System.out.println("error in server side "+e.toString());
+	    System.out.println("QNUM: " + queryNumber+ "ERROR in server side "+e.toString());
 	}
 	return uname_db;
 		
